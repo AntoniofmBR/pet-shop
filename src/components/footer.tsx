4592257b0b7d903114e6b988f5c86ebbc138b64c
Antonio Fonseca <antonio.fonse06@gmail.com>
2024-11-22 13:34:31 -0300

    Fix responsivity error

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,92 +6,104 @@ import logo from '../assets/png/logo.png'
 
 export function Footer () {
   return (
-    <footer className='bg-blues-blue_300 flex flex-col justify-center items-center w-full p-3 absolute' >
+    <footer className='bg-blues-blue_300 flex flex-col justify-center items-center w-full p-3 absolute gap-4' >
       <section className='w-full flex justify-between items-center' >
         <motion.img
           src={logo}
           alt="logo"
-          className='h-32 w-auto'
+          className='lg:h-32 md:h-30 sm:h-0 w-auto'
           whileHover={{ scale: 1.1 }}
         />
 
-<div className='flex gap-7 lg:text-2xl md:text-2xl sm:text-base font-bold' >
-        <Link
-          to='homePage'
-          smooth={ true }
-          duration={ 500 }
-        >
-          <motion.button
-          whileHover={{ scale: 1.1 }}
+        <div className='flex gap-7 lg:text-2xl md:text-2xl sm:text-sm font-bold' >
+          <Link
+            to='homePage'
+            smooth={ true }
+            duration={ 500 }
           >
-            Home
-          </motion.button>
-        </Link>
+            <motion.button
+            whileHover={{ scale: 1.1 }}
+            >
+              Home
+            </motion.button>
+          </Link>
 
-        <Link
-          to='aboutPage'
-          smooth={ true }
-          duration={ 500 }
-         >
-          <motion.button
-          whileHover={{ scale: 1.1 }}
+          <Link
+            to='aboutPage'
+            smooth={ true }
+            duration={ 500 }
           >
-            Sobre nós
-          </motion.button>
-        </Link>
+            <motion.button
+            whileHover={{ scale: 1.1 }}
+            >
+              Sobre nós
+            </motion.button>
+          </Link>
 
-        <Link
-          to='productsPage'
-          smooth={ true }
-          duration={ 500 }
-         >
-          <motion.button
-          whileHover={{ scale: 1.1 }}
+          <Link
+            to='productsPage'
+            smooth={ true }
+            duration={ 500 }
           >
-            Produtos
-          </motion.button>
-        </Link>
+            <motion.button
+            whileHover={{ scale: 1.1 }}
+            >
+              Produtos
+            </motion.button>
+          </Link>
 
-        <Link
-          to='contactPage'
-          smooth={ true }
-          duration={ 500 }
-         >
-          <motion.button
-          whileHover={{ scale: 1.1 }}
+          <Link
+            to='contactPage'
+            smooth={ true }
+            duration={ 500 }
           >
-            Contato
-          </motion.button>
-        </Link>
-      </div>
+            <motion.button
+            whileHover={{ scale: 1.1 }}
+            >
+              Contato
+            </motion.button>
+          </Link>
+        </div>
 
         <div className='flex gap-4' >
           <motion.div 
-            className='h-12 w-12 flex items-center justify-center rounded-full bg-gradient-to-r from-yellows-yellow_100 via-pinks-pink_100 to-purples-purple_200'
+            className='lg:h-12 lg:w-12 md:h-10 md:w-10 sm:h-6 sm:w-6 flex items-center justify-center rounded-full bg-gradient-to-r from-yellows-yellow_100 via-pinks-pink_100 to-purples-purple_200'
             whileHover={{ scale: 1.07 }}
           >
-            <InstagramLogo weight='bold' size={32} />
+            <InstagramLogo
+              weight='bold'
+              className='lg:h-7 lg:w-7 md:h-6 md:w-6 sm:h-4 sm:w-4'
+            />
           </motion.div>
 
           <motion.div
-          className='h-12 w-12 flex items-center justify-center rounded-full bg-reds-red_100'
+          className='lg:h-12 lg:w-12 md:h-10 md:w-10 sm:h-6 sm:w-6 flex items-center justify-center rounded-full bg-reds-red_100'
           whileHover={{ scale: 1.07 }}
           >
-            <YoutubeLogo weight='bold' size={32} />
+            <YoutubeLogo
+              weight='bold'
+              className='lg:h-7 lg:w-7 md:h-6 md:w-6 sm:h-4 sm:w-4'
+            />
           </motion.div>
 
           <motion.div 
-          className='h-12 w-12 flex items-center justify-center rounded-full bg-grays-gray_500'
+          className='lg:h-12 lg:w-12 md:h-10 md:w-10 sm:h-6 sm:w-6 flex items-center justify-center rounded-full bg-grays-gray_500'
           whileHover={{ scale: 1.07 }}
           >
-          <XLogo weight='bold' size={32} />
+          <XLogo
+            weight='bold'
+            className='lg:h-7 lg:w-7 md:h-6 md:w-6 sm:h-4 sm:w-4'
+          />
           </motion.div>
 
           <motion.div
-          className='h-12 w-12 flex items-center justify-center rounded-full bg-blues-blue_200'
+          className='lg:h-12 lg:w-12 md:h-10 md:w-10 sm:h-6 sm:w-6 flex items-center justify-center rounded-full bg-blues-blue_200'
           whileHover={{ scale: 1.07 }}
           >
-          <FacebookLogo weight='bold' size={32} />
+          <FacebookLogo
+            weight='bold'
+            className='lg:h-7 lg:w-7 md:h-6 md:w-6 sm:h-4 sm:w-4'
+          />
           </motion.div>
         </div>
       </section>
